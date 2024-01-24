118a1f359cdb40e346510059126936c1924d875f
leobel99 <leobel99@gmail.com>
2024-01-24 13:29:56 +0100

    DB ops for shape predictor and face detector

--- website/__init__.py
+++ website/__init__.py
@@ -12,7 +12,10 @@ def create_app():
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-
+    current_directory = path.dirname(path.abspath(__file__))
+    project_directory = path.dirname(current_directory)
+    shape_predictor_path = path.join(project_directory, r'shape_predictor_68_face_landmarks_GTX.dat')
+    face_detector_path = path.join(project_directory, r'mmod_human_face_detector.dat')
 
     from .views import views
     from .auth import auth
@@ -20,10 +23,28 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Video
+    from .models import User, Video, File
 
     with app.app_context():
         db.create_all()
+
+        if not File.query.filter(
+                db.or_(File.filename == shape_predictor_path,
+                       File.filename == face_detector_path)).first():
+            with open(shape_predictor_path, 'rb') as file:
+                content = file.read()
+                file1 = File(filename=shape_predictor_path,
+                             content=content)
+                db.session.add(file1)
+
+            with open(face_detector_path, 'rb') as file:
+                content = file.read()
+                file2 = File(filename=face_detector_path, content=content)
+                db.session.add(file2)
+
+            db.session.commit()
+        else:
+            pass
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
